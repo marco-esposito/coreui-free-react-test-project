681e9c1c98db0538751c98fed5f1c5a60faaf2c7
marco-esposito <m.espo@tiscali.it>
2018-05-28 15:06:16 +0200

    refactoring

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -3,15 +3,7 @@ import { combineReducers } from 'redux';
 import postsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
-<<<<<<< HEAD
   postsState: postsReducer,
-=======
-<<<<<<< HEAD
-  postsState: postsReducer,
-=======
-  posts: postsReducer,
->>>>>>> 9942c9c11dc8956bf7f0d256a70d377cc0fb140d
->>>>>>> a5898df409d6537191a4fabb1b7d4f80bef3500c
 });
 
 export default rootReducer;
--- src/store/reducers/reducer_posts.js
+++ src/store/reducers/reducer_posts.js
@@ -1,17 +1,7 @@
 const initialState = {
-<<<<<<< HEAD
   posts: {
     1: 'hey!'
   }
-=======
-<<<<<<< HEAD
-  posts: {
-    1: 'hey!'
-  }
-=======
-  message: 'hey!'
->>>>>>> 9942c9c11dc8956bf7f0d256a70d377cc0fb140d
->>>>>>> a5898df409d6537191a4fabb1b7d4f80bef3500c
 };
 
 export default (state = initialState, action) => {
